6e9f3d4389551865936755c0918f1460e1cc259d
minghuaw <michael.wu1107@gmail.com>
2021-04-02 16:30:22 -0700

    updated error impl for http_warp

--- toy-rpc/src/transport/ws/warp_ws.rs
+++ toy-rpc/src/transport/ws/warp_ws.rs
@@ -9,15 +9,20 @@ where
     async fn read_payload(&mut self) -> Option<Result<Vec<u8>, Error>> {
         let msg = self.next().await?;
         match msg {
-            Err(e) => return Some(Err(Error::TransportError(e.to_string()))),
+            Err(e) => return Some(Err(Error::IoError(
+                std::io::Error::new(ErrorKind::InvalidData, e.to_string())
+            ))),
             Ok(m) => {
                 if m.is_close() {
                     return None;
                 } else if m.is_binary() {
                     return Some(Ok(m.into_bytes()));
                 }
-                Some(Err(Error::TransportError(
-                    "Expecting WebSocket::Message::Binary, but found something else".to_string(),
+                Some(Err(Error::IoError(
+                    std::io::Error::new(
+                    ErrorKind::InvalidData,
+                    "Expecting WebSocket::Message::Binary, but found something else".to_string()
+                    )
                 )))
             }
         }
@@ -28,7 +33,7 @@ where
 impl<S, E> PayloadWrite for S
 where
     S: Sink<warp::ws::Message, Error = E> + Send + Sync + Unpin,
-    E: std::error::Error + 'static,
+    E: std::error::Error + Send  + Sync + 'static,
 {
     async fn write_payload(&mut self, payload: Vec<u8>) -> Result<(), Error> {
         // let msg = WsMessage::Binary(payload.into());
@@ -36,6 +41,6 @@ where
 
         self.send(msg)
             .await
-            .map_err(|e| Error::TransportError(e.to_string()))
+            .map_err(|e| Error::Internal(Box::new(e)))
     }
 }
