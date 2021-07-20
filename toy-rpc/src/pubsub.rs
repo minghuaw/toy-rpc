5084b13b72fd18da7f2c25f8ff9bdbf1320e80f8
minghuaw <michael.wu1107@gmail.com>
2021-07-20 14:01:39 -0700

    added type state for AckMode

--- toy-rpc/src/pubsub.rs
+++ toy-rpc/src/pubsub.rs
@@ -9,3 +9,13 @@ pub trait Topic {
     /// Name of the topic
     fn topic() -> String;
 }
+
+/// Type state for AckMode. No Ack will be sent back upon recving a new Publish item
+pub struct AckModeNone { }
+
+/// Type state for AckMode. One Ack message will be sent back automatically upon recving a new Publish item
+pub struct AckModeAuto { }
+
+/// Type state for AckMode. This is only allowed on Client. 
+/// The client will need to manually Ack the delivery of a new Publish item
+pub struct AckModeManual { }
