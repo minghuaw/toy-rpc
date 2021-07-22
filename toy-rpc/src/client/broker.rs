20c3568db9c70c4d1e0019e5d33d40bc1453d106
minghuaw <michael.wu1107@gmail.com>
2021-07-22 01:08:51 -0700

    created handlers for ClientBrokerItem

--- toy-rpc/src/client/broker.rs
+++ toy-rpc/src/client/broker.rs
@@ -96,6 +96,181 @@ pub(crate) struct ClientBroker {
     all(feature = "tokio_runtime", not(feature = "async_std_runtime")),
     all(feature = "async_std_runtime", not(feature = "tokio_runtime"))
 ))]
+impl ClientBroker {
+    async fn handle_request<'w, W>(
+        &'w mut self, 
+        writer: &'w mut W,
+        id: MessageId, 
+        service_method: String, 
+        duration: Duration, 
+        body: Box<OutboundBody>, 
+        resp_tx: oneshot::Sender<Result<ResponseResult, Error>>
+    ) -> Result<(), Error> 
+    where 
+        W: Sink<ClientWriterItem, Error = flume::SendError<ClientWriterItem>> + Send + Unpin,
+    {
+        // fetch_add returns the previous value
+        let (tx, rx) = oneshot::channel();
+        let fut = async move {
+            // takes care of receiving/cancel  error
+            match rx.await {
+                Ok(res) => res,
+                Err(_) => Err(Error::Canceled(Some(id))),
+            }
+        };
+        let request_result = writer
+            .send(ClientWriterItem::Request(
+                id,
+                service_method,
+                duration,
+                body,
+            ))
+            .await;
+
+        task::spawn(async move {
+            #[cfg(all(feature = "tokio_runtime", not(feature = "async_std_runtime")))]
+            let timout_result = ::tokio::time::timeout(duration, fut).await;
+            #[cfg(all(feature = "async_std_runtime", not(feature = "tokio_runtime")))]
+            let timout_result = ::async_std::future::timeout(duration, fut).await;
+
+            let cancellation_result = match timout_result {
+                Ok(res) => res,
+                Err(_) => {
+                    if let Err(_) = resp_tx.send(Err(Error::Timeout(Some(id)))) {
+                        log::trace!("InternalError: Unable to send Error::Timeout(Some({})) over response channel, response receiver is dropped", id);
+                    }
+                    return;
+                }
+            };
+            match cancellation_result {
+                Ok(res) => {
+                    let response_result = Ok(res);
+                    resp_tx.send(response_result)
+                        .unwrap_or_else(|_| log::trace!("InternalError: Unable to send RPC response over response channel, response receiver is dropped"));
+                }
+                Err(_) => {
+                    // RPC request is already canceled, simply return
+                    return;
+                }
+            };
+        });
+
+        self.pending.insert(id, tx);
+        request_result.map_err(|err| err.into())
+    }
+
+    async fn handle_response(&mut self, id: MessageId, result: ResponseResult) -> Result<(), Error> {
+        if let Some(tx) = self.pending.remove(&id) {
+            tx.send(Ok(result)).map_err(|_| {
+                Error::Internal(
+                    "InternalError: client failed to send response over channel".into(),
+                )
+            })
+        } else {
+            Err(Error::Internal(
+                format!("InternalError: Response channel not found for id: {}", id).into(),
+            ))
+        }
+    }
+
+    async fn handle_cancel<'w, W>(&'w mut self, writer: &'w mut W, id: MessageId) -> Result<(), Error> 
+    where 
+        W: Sink<ClientWriterItem, Error = flume::SendError<ClientWriterItem>> + Send + Unpin,
+    {
+        if let Some(tx) = self.pending.remove(&id) {
+            tx.send(Err(Error::Canceled(Some(id))))
+                .map_err(|_| Error::Internal(
+                    format!(
+                        "Unable to send Error::Canceled(Some({})) over response channel",
+                        id
+                    )
+                    .into(),
+                ))?;
+        }
+        writer
+            .send(ClientWriterItem::Cancel(id))
+            .await
+            .map_err(|err| err.into())
+    }
+
+    async fn handle_publish<'w, W>(&'w mut self, writer: &'w mut W, topic: String, body: Box<OutboundBody>) -> Result<(), Error> 
+    where 
+        W: Sink<ClientWriterItem, Error = flume::SendError<ClientWriterItem>> + Send + Unpin,
+    {
+        let id = self.count.fetch_add(1, Ordering::Relaxed);
+                // TODO: QoS check? at least once?
+        let res = writer
+            .send(ClientWriterItem::Publish(id, topic, body))
+            .await
+            .map_err(|err| err.into());
+
+        // TODO: Spawn a timed task to check Ack?
+        res
+    }
+
+    async fn handle_subscribe<'w, W>(&'w mut self, writer: &'w mut W, topic: String, item_sink: Sender<Box<InboundBody>>) -> Result<(), Error> 
+    where 
+        W: Sink<ClientWriterItem, Error = flume::SendError<ClientWriterItem>> + Send + Unpin,
+    {
+        let id = self.count.fetch_add(1, Ordering::Relaxed);
+        // NOTE: Only one local subscriber is allowed
+        self.subscriptions.insert(topic.clone(), item_sink);
+
+        let res = writer
+            .send(ClientWriterItem::Subscribe(id, topic))
+            .await
+            .map_err(|err| err.into());
+        // TODO: Spawn a timed task to check Ack?
+        res
+    }
+
+    fn handle_new_local_subscriber(&mut self, topic: String, new_item_sink: Sender<Box<InboundBody>>) -> Result<(), Error> {
+        self.subscriptions.insert(topic, new_item_sink);
+        Ok(())
+    }
+
+    async fn handle_unsubscribe<'w, W>(&'w mut self, writer: &'w mut W, topic: String) -> Result<(), Error> 
+    where 
+        W: Sink<ClientWriterItem, Error = flume::SendError<ClientWriterItem>> + Send + Unpin,
+    {
+        let id = self.count.fetch_add(1, Ordering::Relaxed);
+        // NOTE: the sender should be dropped on the Client side
+        let res = writer
+            .send(ClientWriterItem::Unsubscribe(id, topic))
+            .await
+            .map_err(|err| err.into());
+        // TODO: Spawn  timed task to check Ack?
+        res
+    }
+
+    fn handle_subscription(&mut self, _: MessageId, topic: String, item: Box<InboundBody>) -> Result<(), Error> {
+        if let Some(sub) = self.subscriptions.get(&topic) {
+            match sub.try_send(item) {
+                Ok(_) => Ok(()),
+                Err(err) => match err {
+                    flume::TrySendError::Disconnected(_) => {
+                        self.subscriptions.remove(&topic);
+                        Err(Error::Internal(
+                            "Subscription recver is Disconnected".into(),
+                        ))
+                    }
+                    _ => Ok(()),
+                },
+            }
+        } else {
+            Err(Error::Internal("Topic is not found locally".into()))
+        }
+    }
+
+    async fn handle_ack(&mut self) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
+
+#[cfg(any(
+    all(feature = "tokio_runtime", not(feature = "async_std_runtime")),
+    all(feature = "async_std_runtime", not(feature = "tokio_runtime"))
+))]
 #[async_trait::async_trait]
 impl brw::Broker for ClientBroker {
     type Item = ClientBrokerItem;
@@ -120,154 +295,34 @@ impl brw::Broker for ClientBroker {
                 body,
                 resp_tx,
             } => {
-                // fetch_add returns the previous value
-                // let id = self.count.fetch_add(1, Ordering::Relaxed);
-                let (tx, rx) = oneshot::channel();
-                let fut = async move {
-                    // takes care of receiving/cancel  error
-                    match rx.await {
-                        Ok(res) => res,
-                        Err(_) => Err(Error::Canceled(Some(id))),
-                    }
-                };
-                let request_result = writer
-                    .send(ClientWriterItem::Request(
-                        id,
-                        service_method,
-                        duration,
-                        body,
-                    ))
-                    .await;
-
-                task::spawn(async move {
-                    #[cfg(all(feature = "tokio_runtime", not(feature = "async_std_runtime")))]
-                    let timout_result = ::tokio::time::timeout(duration, fut).await;
-                    #[cfg(all(feature = "async_std_runtime", not(feature = "tokio_runtime")))]
-                    let timout_result = ::async_std::future::timeout(duration, fut).await;
-
-                    let cancellation_result = match timout_result {
-                        Ok(res) => res,
-                        Err(_) => {
-                            if let Err(_) = resp_tx.send(Err(Error::Timeout(Some(id)))) {
-                                log::trace!("InternalError: Unable to send Error::Timeout(Some({})) over response channel, response receiver is dropped", id);
-                            }
-                            return;
-                        }
-                    };
-                    match cancellation_result {
-                        Ok(res) => {
-                            let response_result = Ok(res);
-                            resp_tx.send(response_result)
-                                .unwrap_or_else(|_| log::trace!("InternalError: Unable to send RPC response over response channel, response receiver is dropped"));
-                        }
-                        Err(_) => {
-                            // RPC request is already canceled, simply return
-                            return;
-                        }
-                    };
-                });
-
-                self.pending.insert(id, tx);
-                request_result.map_err(|err| err.into())
+                self.handle_request(&mut writer, id, service_method, duration, body, resp_tx).await
             }
             ClientBrokerItem::Response { id, result } => {
-                if let Some(tx) = self.pending.remove(&id) {
-                    tx.send(Ok(result)).map_err(|_| {
-                        Error::Internal(
-                            "InternalError: client failed to send response over channel".into(),
-                        )
-                    })
-                } else {
-                    Err(Error::Internal(
-                        format!("InternalError: Response channel not found for id: {}", id).into(),
-                    ))
-                }
+                self.handle_response(id, result).await
             },
             ClientBrokerItem::Cancel(id) => {
-                if let Some(tx) = self.pending.remove(&id) {
-                    if let Err(_) = tx.send(Err(Error::Canceled(Some(id)))) {
-                        return Running::Continue(Err(Error::Internal(
-                            format!(
-                                "Unable to send Error::Canceled(Some({})) over response channel",
-                                id
-                            )
-                            .into(),
-                        )));
-                    }
-                }
-                writer
-                    .send(ClientWriterItem::Cancel(id))
-                    .await
-                    .map_err(|err| err.into())
+                self.handle_cancel(&mut writer, id).await
             },
             ClientBrokerItem::Publish { topic, body } => {
-                let id = self.count.fetch_add(1, Ordering::Relaxed);
-                // TODO: QoS check? at least once?
-                let res = writer
-                    .send(ClientWriterItem::Publish(id, topic, body))
-                    .await
-                    .map_err(|err| err.into());
-
-                // TODO: Spawn a timed task to check Ack?
-                // task::spawn(async move {
-
-                // });
-                res
+                self.handle_publish(&mut writer, topic, body).await
             },
             ClientBrokerItem::Subscribe { topic, item_sink } => {
-                let id = self.count.fetch_add(1, Ordering::Relaxed);
-                // NOTE: Only one local subscriber is allowed
-                self.subscriptions.insert(topic.clone(), item_sink);
-
-                let res = writer
-                    .send(ClientWriterItem::Subscribe(id, topic))
-                    .await
-                    .map_err(|err| err.into());
-                // TODO: Spawn a timed task to check Ack?
-                res
+                self.handle_subscribe(&mut writer, topic, item_sink).await
             },
             ClientBrokerItem::NewLocalSubscriber {
                 topic,
                 new_item_sink,
             } => {
-                self.subscriptions.insert(topic, new_item_sink);
-                Ok(())
+                self.handle_new_local_subscriber(topic, new_item_sink)
             },
             ClientBrokerItem::Unsubscribe { topic } => {
-                let id = self.count.fetch_add(1, Ordering::Relaxed);
-                // NOTE: the sender should be dropped on the Client side
-                let res = writer
-                    .send(ClientWriterItem::Unsubscribe(id, topic))
-                    .await
-                    .map_err(|err| err.into());
-                // TODO: Spawn  timed task to check Ack?
-                res
+                self.handle_unsubscribe(&mut writer, topic).await
             },
             ClientBrokerItem::Subscription { id, topic, item } => {
-                log::info!(
-                    "Received subscription message {{id: {}, topic: {}}}",
-                    id,
-                    &topic
-                );
-                if let Some(sub) = self.subscriptions.get(&topic) {
-                    match sub.try_send(item) {
-                        Ok(_) => Ok(()),
-                        Err(err) => match err {
-                            flume::TrySendError::Disconnected(_) => {
-                                self.subscriptions.remove(&topic);
-                                Err(Error::Internal(
-                                    "Subscription recver is Disconnected".into(),
-                                ))
-                            }
-                            _ => Ok(()),
-                        },
-                    }
-                } else {
-                    Err(Error::Internal("Topic is not found locally".into()))
-                }
+                self.handle_subscription(id, topic, item)
             }
             ClientBrokerItem::Ack => {
-                unimplemented!()
+                self.handle_ack().await
             },
             ClientBrokerItem::Stop => {
                 if let Err(err) = writer.send(ClientWriterItem::Stop).await {
