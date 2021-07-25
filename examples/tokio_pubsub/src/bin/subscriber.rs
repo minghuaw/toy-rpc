951a4441e9b0cbe0365bea5f13d578884b4d9b7c
minghuaw <michael.wu1107@gmail.com>
2021-07-25 01:10:04 -0700

    tested ack mode manual on client side

--- examples/tokio_pubsub/src/bin/subscriber.rs
+++ examples/tokio_pubsub/src/bin/subscriber.rs
@@ -9,14 +9,15 @@ async fn main() {
 
     env_logger::init();
     let mut client = Client::builder()
-        .set_ack_mode_auto()
+        .set_ack_mode_manual()
         .dial(ADDR).await.unwrap();
     // let mut client = Client::dial(ADDR).await.unwrap();
     let mut count_sub = client.subscriber::<Count>(10).unwrap();
 
     for _ in 0..30 {
-        if let Some(item) = count_sub.next().await {
-            let item = item.unwrap();
+        if let Some(result) = count_sub.next().await {
+            let delivery = result.unwrap();
+            let item = delivery.ack().await.unwrap();
             println!("{:?}", item);
         } else {
             break;
