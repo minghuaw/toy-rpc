d51daa9b53f2c086e3b7bedc3fbc725502ab592a
minghuaw <michael.wu1107@gmail.com>
2021-07-28 18:12:04 -0700

    updated pubsub example

--- examples/tokio_pubsub/src/lib.rs
+++ examples/tokio_pubsub/src/lib.rs
@@ -6,7 +6,8 @@ pub const ADDR: &str = "127.0.0.1:23333";
 
 #[derive(Debug, Topic, Serialize, Deserialize)]
 #[topic(rename="C", item="u32")]
-pub struct Count(pub u32);
+// pub struct Count(pub u32);
+pub struct Count { }
 
 // impl Topic for Count {
 //     type Item = Count;
